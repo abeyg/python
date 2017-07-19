6c92b78a09ff636dd4bab20d9f7102eb7636e35f
abeyg <software.abey@gmail.com>
2017-07-18 22:28:54 -0400

    Add files via upload

---
+++ hms2dec.py
@@ -0,0 +1,22 @@
+# Write your hms2dec and dms2dec functions here
+def hms2dec(h, m, s):
+  return (h + m/60 + s/3600)*360/24
+
+def dms2dec(d, m, s):
+  if d < 0:
+     return (d - m/60 - s/3600)
+  else:
+     return (d + m/60 + s/3600)
+   
+
+# You can use this to test your function.
+# Any code inside this `if` statement will be ignored by the automarker.
+if __name__ == '__main__':
+  # The first example from the question
+  print(hms2dec(23, 12, 6))
+
+  # The second example from the question
+  print(dms2dec(22, 57, 18))
+
+  # The third example from the question
+  print(dms2dec(-66, 5, 5.1))
